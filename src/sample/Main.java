0eb1f7de809ce78c50b9549d81278cff5291f3d9
Leo! <leo@leoleo.com>
2015-12-03 17:32:36 +0100

    Commit 9:
    Push de main y sample.fxml (ver commit 7)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,9 +12,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(new Group(), 500, 400);
+        scene.getStylesheets().add("/DarkTheme.css");
         primaryStage.setTitle("El Temps"); //canviem el nom.
-        primaryStage.setScene(new Scene(root, 600, 500)); //canviem la mida
+        primaryStage.setScene(new Scene(root, 285, 500)); //canviem la mida
         primaryStage.show();
+
+
+
     }
 
 
